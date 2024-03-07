d02ffaf95f9b78e781aa9d9295e666b17ba1f2a7
mafuth <mafaaz708@gmail.com>
2024-03-08 02:00:02 +0500

    fixed images not showing in build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,7 +25,7 @@
 				</div>
 				<p class="rounded-md bg-zinc-900 p-3 font-[Fredoka] text-center text-2xl mt-3">MAFUTH</p>
 			</div>
-			<div class="rounded-lg border-none bg-[url(./DJI_0056.jpg)] bg-center bg-cover hover:bg-bottom ease-in-out duration-300">
+			<div class="rounded-lg border-none bg-[url(https://raw.githubusercontent.com/mafuth/mySite/main/static/DJI_0056.jpg)] bg-center bg-cover hover:bg-bottom ease-in-out duration-300">
 				<div class="backdrop-blur-sm relative w-100 z-[2] bg-black bg-opacity-80 p-10">
 					<h1 class="font-extrabold font-[Fredoka] text-2xl">Web Developer &  Data Analyst</h1>
 					<p class="rounded-md bg-zinc-800 p-3 mt-4 font-[Nerko]">Creating inovative soilutions to big problems using my skills.</p>
@@ -38,7 +38,7 @@
 			</div>
 		</div>
 		<div class="lg:mx-[200px] mx-3 mt-3 grid grid-cols-1 gap-3 text-white group">
-			<div class="rounded-lg border-none bg-[url(./DJI_0053.jpg)] bg-center bg-cover group-hover:bg-top ease-in-out duration-300">
+			<div class="rounded-lg border-none bg-[url(https://raw.githubusercontent.com/mafuth/mySite/main/static/DJI_0053.jpg)] bg-center bg-cover group-hover:bg-top ease-in-out duration-300">
 				<div class="backdrop-blur-sm relative w-100 z-[2] grid content-center justify-items-center bg-black bg-opacity-80 p-10">
 					<h1 class="font-extrabold font-[Fredoka] text-4xl">My Skills</h1>
 					<div class="lg:mx-[200px] mx-3 mt-3 grid lg:grid-cols-2 grid-cols-1 gap-3 text-white">
@@ -128,7 +128,7 @@
 		<h1 class="text-white font-extrabold lg:text-6xl text-2xl font-[Fredoka]">What am I up to?</h1>
 	</div>
 	<div class="lg:mx-[200px] mx-3 mt-10 grid lg:grid-cols-2 grid-cols-1 gap-3 text-white">
-		<div class="rounded-lg border-none bg-[url(./chronicalBot.jpg)] bg-center bg-cover hover:bg-left ease-in-out duration-300">
+		<div class="rounded-lg border-none bg-[url(https://raw.githubusercontent.com/mafuth/mySite/main/static/chronicalBot.jpg)] bg-center bg-cover hover:bg-left ease-in-out duration-300">
 			<div class="backdrop-blur-sm relative w-100 h-full z-[2] bg-black bg-opacity-90 p-10">
 				<h1 class="font-extrabold font-[Fredoka] text-2xl">Chronical BOT</h1>
 				<p class="rounded-md bg-zinc-800 p-3 mt-4 font-[Nerko]">News scrapping summerization and adding the "ENGAGMENT" factor using a self trained local LLM.</p>
@@ -140,7 +140,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="rounded-lg border-none bg-[url(./chaosStream.png)] bg-center bg-cover hover:bg-right ease-in-out duration-300">
+		<div class="rounded-lg border-none bg-[url(https://raw.githubusercontent.com/mafuth/mySite/main/static/chaosStream.png)] bg-center bg-cover hover:bg-right ease-in-out duration-300">
 			<div class="backdrop-blur-sm relative w-100 h-full z-[2] bg-black bg-opacity-90 p-10">
 				<h1 class="font-extrabold font-[Fredoka] text-2xl">Chaos Stream</h1>
 				<p class="rounded-md bg-zinc-800 p-3 mt-4 font-[Nerko]">A fully automated streaming platform that can do video transcoding on the fly and automated torrent search and download for movies and shows, all inside Docker.</p>
